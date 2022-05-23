0a463d773a83b8d8ec5b6a571d52b7b7da601c13
Olivia Lind <eo.lind@gmail.com>
2022-05-23 14:07:18 -0500

    phase 7 complete

--- Program.cs
+++ Program.cs
@@ -8,14 +8,28 @@ namespace GuessingGame
     {
         static void Main()
         {
+            Console.WriteLine("Choose a difficulty level: Easy, Medium, or Hard");
+
+            string difficulty = Console.ReadLine();
+
+            int numberOfGuesses = 0;
+
+            if (difficulty == "easy") {
+                numberOfGuesses = 8;
+            } else if (difficulty == "medium") {
+                numberOfGuesses = 6;
+            } else if (difficulty == "hard") {
+                numberOfGuesses = 4;
+            }
+
             Console.WriteLine("Guess the secret number! ");
             int secretNumber = new Random().Next(1, 101);
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 1; i < numberOfGuesses; i++)
             {
                 Console.WriteLine($"correct answer for testing purposes: {secretNumber}");
 
-                Console.WriteLine($"You have four guesses.You are on guess #{i + 1}.");
+                Console.WriteLine($"You have {numberOfGuesses} guesses. You are on guess #{i}.");
                 int guess = Int32.Parse(Console.ReadLine());
 
                 if (guess == secretNumber)
