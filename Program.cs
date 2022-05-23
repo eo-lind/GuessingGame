1101615a877fa0797bc5af496db287522e225c1b
Olivia Lind <eo.lind@gmail.com>
2022-05-23 13:02:05 -0500

    phase one complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Guess the secret number! ");
+            int guess = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(guess);
+        }
+    }
+    
+}
