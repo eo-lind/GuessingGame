9766774ea9dad1d5926a5d9acd57e35c54e5de63
Olivia Lind <eo.lind@gmail.com>
2022-05-23 15:08:30 -0500

    phase 8 complete

--- Program.cs
+++ Program.cs
@@ -8,30 +8,63 @@ namespace GuessingGame
     {
         static void Main()
         {
-            Console.WriteLine("Choose a difficulty level: Easy, Medium, or Hard");
+            Console.WriteLine("Choose a difficulty level: Easy, Medium, Hard, or Cheater");
 
             string difficulty = Console.ReadLine().ToLower();
 
             int numberOfGuesses = 0;
-
-            if (difficulty == "easy") {
-                numberOfGuesses = 8;
-            } else if (difficulty == "medium") {
-                numberOfGuesses = 6;
-            } else if (difficulty == "hard") {
-                numberOfGuesses = 4;
-            }
-
-            Console.WriteLine("Guess the secret number! ");
+            int guess = new int();
             int secretNumber = new Random().Next(1, 101);
 
-            for (int i = 1; i < numberOfGuesses; i++)
+            if (difficulty == "easy")
+            {
+                numberOfGuesses = 8;
+            }
+            else if (difficulty == "medium")
+            {
+                numberOfGuesses = 6;
+            }
+            else if (difficulty == "hard")
+            {
+                numberOfGuesses = 4;
+            }
+            else if (difficulty == "cheater")
+            {
+
+                while (secretNumber != guess)
+                {
+                    {
+                        Console.WriteLine("Guess the secret number! ");
+                        guess = Int32.Parse(Console.ReadLine());
+                    };
+
+                    if (secretNumber < guess)
+                    {
+                        Console.WriteLine("Guess lower.");
+
+                    }
+                    else if (secretNumber > guess)
+                    {
+                        Console.WriteLine("Guess higher.");
+                    }
+                }
+
+
+                Console.WriteLine("You got it!");
+                return;
+            };
+
+
+            Console.WriteLine("Guess the secret number! ");
+
+
+            for (int i = 1; i <= numberOfGuesses; i++)
             {
                 Console.WriteLine($"correct answer for testing purposes: {secretNumber}");
 
                 Console.WriteLine($"You have {numberOfGuesses} guesses. You are on guess #{i}.");
-                
-                int guess = Int32.Parse(Console.ReadLine());
+
+                guess = Int32.Parse(Console.ReadLine());
 
                 if (guess == secretNumber)
                 {
@@ -40,15 +73,18 @@ namespace GuessingGame
                 }
                 else
                 {
-                    if (secretNumber > guess) {
+                    if (secretNumber > guess)
+                    {
                         Console.WriteLine("Guess higher!");
-                    } else {
+                    }
+                    else
+                    {
                         Console.WriteLine("Guess lower.");
                     }
 
-                }
+                };
 
-            }
+            };
 
         }
     }
