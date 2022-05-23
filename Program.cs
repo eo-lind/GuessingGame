10368d172b7c93ce8af06edd5f858e47124fed5e
Olivia Lind <eo.lind@gmail.com>
2022-05-23 13:49:59 -0500

    phase 5 complete

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@ namespace GuessingGame
         static void Main()
         {
             Console.WriteLine("Guess the secret number! ");
-            int secretNumber = 42;
+            int secretNumber = new Random().Next(1, 101);
 
             for (int i = 0; i < 4; i++)
             {
-
+                // Console.WriteLine($"correct answer: {secretNumber}");
                 int guess = Int32.Parse(Console.ReadLine());
 
                 if (guess == secretNumber)
@@ -23,7 +23,7 @@ namespace GuessingGame
                 }
                 else
                 {
-                    Console.WriteLine($"Sorry, wrong number. This was guess #({i + 1}) of 4.");
+                    Console.WriteLine($"Sorry, wrong number. This was guess #{i + 1} of 4.");
                 }
 
             }
